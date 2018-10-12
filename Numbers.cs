c69c61900ee02a5f781bf246c9d627ef9028e13a
Joshua C <campbellsoup246@gmail.com>
2018-10-12 10:17:19 -0400

    Made more Number objects and added division.

--- Numbers.cs
+++ Numbers.cs
@@ -4,16 +4,24 @@ namespace HelloWorld
     public class Numbers
     {
         int a;
-        int b;
+        int b, c, d;
 
-        public Numbers(int _a, int _b)
+        public Numbers(int _a, int _b, int _c, int _d)
         {
             a = _a;
             b = _b;
+            c = _c;
+            d = _d;
         }
 
         public int add() {
             return a + b;
         }
+
+        public int divide()
+        {
+            return a / b;
+        }
+
     }
 }
